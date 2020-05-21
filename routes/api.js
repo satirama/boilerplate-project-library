8a9338c01d13a12439dd63d02584be84c435d810
Angelica Miranda <mirandas.angelica@gmail.com>
2020-05-21 13:41:49 -0500

    add endpoint

--- routes/api.js
+++ routes/api.js
@@ -20,15 +20,57 @@ module.exports = function (app) {
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {
+        db.collection('library').aggregate({
+          $project: {
+            title: 1,
+            _id: 1,
+            commentcount: {
+              $size: "$comments"
+            } 
+          }
+        }, function(err, result) {
+          res.status(200)
+            .send(result);
+        });
+      });
     })
     
     .post(function (req, res){
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
+      var query = {
+        title: title,
+        comments: []
+      };
+    
+      if (title == '' || typeof title != 'string') {
+        res.status(400).send('no title was sent');
+      } else {
+        MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {
+          db.collection('library').insert(query, function(err, result) {
+            db.collection('library').findOne(
+              {_id: new ObjectId(result.insertedIds[0])},
+              {_id: 1, title: 1},
+              (err, doc) => {
+                res.status(200)
+                  .send(doc);
+              });
+          });  
+        });
+      }
     })
     
     .delete(function(req, res){
       //if successful response will be 'complete delete successful'
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {
+        db.collection('library').remove(
+          {},
+          (err, doc) => {
+            if (err) res.status(500).type('text').send('could not delete books');
+            res.status(200).type('text').send('complete delete successful');
+        });
+      });
     });
 
 
@@ -37,17 +79,59 @@ module.exports = function (app) {
     .get(function (req, res){
       var bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      var query = {
+        _id: new ObjectId(bookid)
+      };
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {
+        db.collection('library').find(query).toArray(function(err, result) {
+          if (result.length < 1) res.status(400).send({message: 'no book exists'});
+          else {
+            res.status(200).send(result);
+          }
+        });  
+      });
     })
     
     .post(function(req, res){
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
+      var query = {
+        _id: new ObjectId(bookid)
+      };
+      var update = {
+        $push: {
+          comments: comment
+        }
+      };
+    
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {
+        db.collection('library').findOneAndUpdate(
+          query,
+          update,
+          {returnNewDocument: true},
+          function(err, result) {
+            if (err) res.status(500).type('text').send('could not update ' + bookid);
+            res.status(200)
+                .send(result.value);
+        });
+      });
+      
     })
     
     .delete(function(req, res){
       var bookid = req.params.id;
-      //if successful response will be 'delete successful'
+      var query = {
+        _id: new ObjectId(bookid)
+      };
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {
+        db.collection('library').removeOne(
+          query,
+          (err, doc) => {
+            if (err) res.status(500).type('text').send('could not delete ' + bookid);
+            res.status(200).type('text').send('deleted successful');
+        });
+      });
     });
   
 };
